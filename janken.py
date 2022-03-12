96caf5612ad6f21138c1fe24b7becc4b29128a03
matsuken0 <global.saromon_sb9@yahoo.co.jp>
2022-03-13 08:31:57 +0900

    update_janken

--- janken.py
+++ janken.py
@@ -1,40 +1,49 @@
-print("1:グー　2:チョキ　3:パー")
-taro = int(input("太郎　1,2,3を入力:"))
-hanako = int(input("花子　1,2,3を入力:"))
-janken = ["あいこ","太郎 win", "花子 win"]
-
-def janken_c(taro, hanako, c="c"):
-    taro -= 1
-    hanako -= 1
-
-    if taro == hanako:
-        print(f"janken_{c} あいこ")
-    elif (taro + 1) % 3 == hanako:
-        print(f"janken_{c} 太郎 win")
-    else:
-        print(f"janken_{c} 花子win")
+class Janken:
+    def __init__(self, name1="太郎", name2="花子"):
+        self.name1 = name1
+        self.name2 = name2
+        kye = {1:"グー", 2:"チョキ", 3:"パー"}
+        print(kye)
+        self.taro = int(input(f"{name1}　1,2,3を入力:"))
+        self.hanako = int(input(f"{name2}　1,2,3を入力:"))
+        self.result = f"{self.name1}は{kye[self.taro]}、{self.name2}は{kye[self.hanako]}で"
 
 
-def janken_b(t, h, b="b"):
-    if t == h:
-        print(f"janken_{b} あいこ")
-    elif (t ==1 and h == 2) or (t ==2 and h == 3) or (t ==3 and h == 1):
-        print(f"janken_{b} 太郎 win")
-    else:
-        print(f"janken_{b} 花子win")
+    def janken_c(self):
+        self.taro -= 1
+        self.hanako -= 1
 
-def janken_d(taro, hanako, c="d"):
-    taro -= 1
-    hanako -= 1
-
-    if taro == hanako:
-        return 0, f"{c}"
-    elif (taro + 1) % 3 == hanako:
-        return 1, f"{c}"
-    else:
-        return 2, f"{c}"
+        if self.taro == self.hanako:
+            print(f"{self.result} あいこ")
+        elif (self.taro + 1) % 3 == self.hanako:
+            print(f"{self.result} {self.name1} win")
+        else:
+            print(f"{self.result} {self.name2} win")
 
 
-# janken_c(taro, hamako)
-j_d =janken_d(taro, hanako, "j_d")
-print(f"func ({j_d[1]}) の結果は {janken[j_d[0]]}")
+    def janken_b(self):
+        if self.taro == self.hanako:
+            print(f"{self.result} あいこ")
+        elif (self.taro ==1 and self.hanako == 2) or (self.taro ==2 and self.hanako == 3) or (self.taro ==3 and self.hanako == 1):
+            print(f"{self.result} {self.name1} win")
+        else:
+            print(f"{self.result} {self.name2} win")
+
+    def janken_d(self):
+        self.taro -= 1
+        self.hanako -= 1
+
+        if self.taro == self.hanako:
+            return 0
+        elif (self.taro + 1) % 3 == self.hanako:
+            return 1
+        else:
+            return 2
+
+
+
+
+janken_base = Janken()
+janken_c = janken_base.janken_c
+janken_b = janken_base.janken_b
+janken_b()

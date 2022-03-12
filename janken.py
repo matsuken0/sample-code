4f9fac8dc1e2a4d0b48897e0067ef2c1968da628
matsuken0 <global.saromon_sb9@yahoo.co.jp>
2022-03-12 15:40:53 +0900

    update janken

--- janken.py
+++ janken.py
@@ -1,15 +1,15 @@
 print("1:グー　2:チョキ　3:パー")
 taro = int(input("太郎　1,2,3を入力:"))
-hamako = int(input("花子　1,2,3を入力:"))
+hanako = int(input("花子　1,2,3を入力:"))
 janken = ["あいこ","太郎 win", "花子 win"]
 
-def janken_c(taro, hamako, c="c"):
+def janken_c(taro, hanako, c="c"):
     taro -= 1
-    hamako -= 1
+    hanako -= 1
 
-    if taro == hamako:
+    if taro == hanako:
         print(f"janken_{c} あいこ")
-    elif (taro + 1) % 3 == hamako:
+    elif (taro + 1) % 3 == hanako:
         print(f"janken_{c} 太郎 win")
     else:
         print(f"janken_{c} 花子win")
@@ -23,18 +23,18 @@ def janken_b(t, h, b="b"):
     else:
         print(f"janken_{b} 花子win")
 
-def janken_d(taro, hamako, c="d"):
+def janken_d(taro, hanako, c="d"):
     taro -= 1
-    hamako -= 1
+    hanako -= 1
 
-    if taro == hamako:
+    if taro == hanako:
         return 0, f"{c}"
-    elif (taro + 1) % 3 == hamako:
+    elif (taro + 1) % 3 == hanako:
         return 1, f"{c}"
     else:
         return 2, f"{c}"
 
 
 # janken_c(taro, hamako)
-j_d =janken_d(taro, hamako, "j_d")
+j_d =janken_d(taro, hanako, "j_d")
 print(f"func ({j_d[1]}) の結果は {janken[j_d[0]]}")

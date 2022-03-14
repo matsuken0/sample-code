cf2d5d01708e5bb779f75fbd2b76f69ef6134e1b
matsuken0 <global.saromon_sb9@yahoo.co.jp>
2022-03-15 00:15:51 +0900

    Person class

--- janken.py
+++ janken.py
@@ -9,7 +9,6 @@ class Janken:
         self.result = f"{self.name1}は{kye[self.taro]}、{self.name2}は{kye[self.hanako]}で"
 
     def janken_a(self):
-        print("A")
         if self.taro == 1:
             if self.hanako == 1:
                 print(f"{self.result} あいこ")
@@ -33,7 +32,6 @@ class Janken:
                 print(f"{self.result} あいこ")
 
     def janken_b(self):
-        print("B")
         if self.taro == self.hanako:
             print(f"{self.result} あいこ")
         elif (self.taro ==1 and self.hanako == 2) or (self.taro ==2 and self.hanako == 3) or (self.taro ==3 and self.hanako == 1):
@@ -62,27 +60,30 @@ class Janken:
         else:
             return self.result, janken_list[2]
 
-def input_name(func):
-    def inner(*args):
-        print("名前を入力してください")
-        name1 = input("プレーヤー1を入力：")
-        name2 = input("プレーヤー2を入力：")
-        type = input("タイプ入力 a,b,c,d:")
-        func(name1,name2,type)
-    return inner
 
-@input_name
-def make_name(name1, name2, type):
-    janken_base = Janken(name1, name2)
-    janken_pattern = {"a":"janken_a", "b":"janken_b", "c":"janken_c", "d":"janken_d"}
-    a = janken_pattern[type]
-    print(a)
-    janken_base.a()
+class Person(Janken):
+    def __init__(self, name1="太郎", name2="花子"):
+        self.name1 = input("プレーヤー１を入力してください:")
+        self.name2 = input("プレーヤー2を入力してください:")
+        kye = {1:"グー", 2:"チョキ", 3:"パー"}
+        print(kye)
+        self.taro = int(input(f"{self.name1}　1,2,3を入力:"))
+        self.hanako = int(input(f"{self.name2}　1,2,3を入力:"))
+        self.result = f"{self.name1}は{kye[self.taro]}、{self.name2}は{kye[self.hanako]}で"
+    pass
 
-make_name()
 
+
+
+#janken_aからc
+janken_base = Janken()
+janken_base.janken_a()
+
+#janken_b専用
 # janken_base = Janken()
-# janken_base.janken_a()
-
 # result, name = janken_base.janken_d()
 # print(result, name)
+
+#プレーヤーの名前入力
+# janken_base = Person()
+# janken_base.janken_a()
